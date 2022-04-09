fc7362a2fab476d94efd1ff88ade551a80433c66
Abhishek Verma <abhishekverma@Abhishek-Verma-B410-Macbook-Pro.local>
2022-04-09 10:52:06 +0530

    formatting

--- src/Components/Borders.js
+++ src/Components/Borders.js
@@ -1,51 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default class Border extends React.Component{
-  constructor(){
+export default class Border extends React.Component {
+  constructor() {
     super();
-    this.state={
-      countries:[],
+    this.state = {
+      countries: [],
     };
   }
 
-  async loadData(){
-    let query=`https://restcountries.com/v2/alpha?codes=`;
-    for(let i=0;i<this.props.borders.length;i++){
-      query+=(this.props.borders[i]+',');
+  async loadData() {
+    let query = `https://restcountries.com/v2/alpha?codes=`;
+    for (let i = 0; i < this.props.borders.length; i++) {
+      query += this.props.borders[i] + ",";
     }
-    const data = await fetch(query,{
-      method:'get'
+    const data = await fetch(query, {
+      method: "get",
     });
     const d = await data.json();
     this.setState({
-      countries:d,
+      countries: d,
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.loadData();
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps !== this.props){
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
       this.loadData();
     }
   }
 
-  render(){
-    return(
-      this.state.countries.length!==0?
-      <div className='countryTiles'>
-        {this.state.countries.map(country=>{
+  render() {
+    return this.state.countries.length !== 0 ? (
+      <div className="countryTiles">
+        {this.state.countries.map((country) => {
           return (
-            <Link className='countryTile' to={`/${country.alpha3Code}`} key={country.alpha3Code}>
-              <img className='countryFlag' src={country.flag} height={100} width={200} alt={country.alpha3Code} />
-              <h3 className='countryTitle'>{country.name}</h3>
+            <Link
+              className="countryTile"
+              to={`/${country.alpha3Code}`}
+              key={country.alpha3Code}
+            >
+              <img
+                className="countryFlag"
+                src={country.flag}
+                height={100}
+                width={200}
+                alt={country.alpha3Code}
+              />
+              <h3 className="countryTitle">{country.name}</h3>
             </Link>
-          )
+          );
         })}
-      </div>:''
+      </div>
+    ) : (
+      ""
     );
   }
 }
